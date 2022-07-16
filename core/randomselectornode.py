b61c5d62260088a2bbb8edb843c8807396bf7bfc
Gavin <gnation15@gmail.com>
2022-07-16 16:05:14 -0500

    Added the random selector node type.

---
+++ core/randomselectornode.py
@@ -0,0 +1,33 @@
+from .node import Node
+import random
+import math
+
+#A random selector child picks a random child to evaluate until it returns SUCCESS
+#We use -1 as a signal that the child is uninitialized so we need to generate a new
+#random currentChild - otherwise we continue execution of the currently running child
+#The scale variable exists to provide a greater range of randomness
+class RandomSelectorNode(Node):
+    def __init__(self, name, scale = 1):
+        super().__init__(name)
+        self.currentChild = -1
+        self.scale = scale
+
+    def display(self, indent):
+        print('\t'*indent + "Random Selector: " + self.name)
+
+        indent += 1
+        for child in self.children:
+            child.display(indent)
+
+    def process(self):
+
+        if self.currentChild == -1:
+            self.currentChild = random.randint(0, len(self.children) * self.scale)
+
+        childstatus = self.children[math.floor(self.currentChild / self.scale)].process()
+
+        if childstatus == Node.STATE_RUNNING:
+            return Node.STATE_RUNNING
+        else:
+            self.currentChild = -1
+            return childstatus        

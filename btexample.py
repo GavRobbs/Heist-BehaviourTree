7a283b8ff7d90321599d6dfadaaaab21be2f318a
Gavin <gnation15@gmail.com>
2022-10-01 17:18:19 -0500

    Created HeistBlackboard class to act as a global blackboard for the behaviour tree.

--- btexample.py
+++ btexample.py
@@ -15,29 +15,32 @@ import time
 import sys
 
 
-class ValuableTheftStatus(Enum):
-    NONE = 0
-    DIAMOND_STOLEN = 1
-    PAINTING_STOLEN = 2
-    ALREADY_SOLD = 3
 
-walletChecked = False
-theftDecided = False
-planDevised = False
-valuableStolen = ValuableTheftStatus.NONE
-vanEscaped = False
-evadedSecurity = False
-notInPrison = True
-balance = 0
+class HeistBlackboard:
 
-def evaluate_checkWallet():
-    global walletChecked
-    if not walletChecked:
-        walletChecked = True
+    class ValuableTheftStatus(Enum):
+        NONE = 0
+        DIAMOND_STOLEN = 1
+        PAINTING_STOLEN = 2
+        ALREADY_SOLD = 3
+
+    def __init__(self):
+        self.walletChecked = False
+        self.theftDecided = False
+        self.planDevised = False
+        self.valuableStolen = HeistBlackboard.ValuableTheftStatus.NONE
+        self.vanEscaped = False
+        self.evadedSecurity = False
+        self.notInPrison = True
+        self.balance = 0
+
+def evaluate_checkWallet(bb):
+    if not bb.walletChecked:
+        bb.walletChecked = True
 
         print("Checking my wallet to see if it's crime time")
-        if balance < 5000000:
-            print(f"Current balance {balance}")
+        if bb.balance < 5000000:
+            print(f"Current balance {bb.balance}")
             print("I'm broke, time to pull off another heist.")
             return Node.STATE_SUCCESS
         else:
@@ -45,152 +48,119 @@ def evaluate_checkWallet():
             print("=====================")
             print("\n\nExecution complete")
             sys.exit(0)
-            walletChecked = False
-            return Node.STATE_FAILED
     else:
         return Node.STATE_SUCCESS
 
-def evaluate_successfulEscape():
-    global evadedSecurity
-    global notInPrison
-
-    if not notInPrison:
+def evaluate_successfulEscape(bb):
+    if not bb.notInPrison:
         return Node.STATE_FAILED
 
     escapeChance = random.randrange(0, 15)
     if escapeChance <= 2:
         print("NOOOO! Got caught by the cops!")
         print("Got sentenced and now I'm languishing in prison for my crimes")
-        evadedSecurity = False
-        notInPrison = False
+        bb.evadedSecurity = False
+        bb.notInPrison = False
         return Node.STATE_FAILED
     else:
         print("I evaded the museum security!")
-        evadedSecurity = True
-        notInPrison = True
+        bb.evadedSecurity = True
+        bb.notInPrison = True
         return Node.STATE_SUCCESS
 
-def evaluate_evadedSecurity():
-    global notInPrison
-
-    if notInPrison:
+def evaluate_evadedSecurity(bb):
+    if bb.notInPrison:
         return Node.STATE_SUCCESS
     else:
         return Node.STATE_FAILED
 
-def tick_decideOnTheft():
-    global theftDecided
-    if not theftDecided:
-        theftDecided = True
+def tick_decideOnTheft(bb):
+    if not bb.theftDecided:
+        bb.theftDecided = True
         print("Made up my mind to steal something")
     else:
         pass
-        #print("Already made up my mind to steal something")
     return Node.STATE_SUCCESS
 
-def tick_devisePlan():
-    global planDevised
-    if not planDevised:
-        planDevised = True
+def tick_devisePlan(bb):
+    if not bb.planDevised:
+        bb.planDevised = True
         print("Devised a plan to steal")
     else:
         pass
         #print("I already devised a plan to steal a diamond")
     return Node.STATE_SUCCESS
 
-def tick_stealDiamond():
-    global valuableStolen
-    if valuableStolen == ValuableTheftStatus.NONE:
-        valuableStolen = ValuableTheftStatus.DIAMOND_STOLEN
+def tick_stealDiamond(bb):
+    if bb.valuableStolen == HeistBlackboard.ValuableTheftStatus.NONE:
+        bb.valuableStolen = HeistBlackboard.ValuableTheftStatus.DIAMOND_STOLEN
         print("Managed to steal the diamond!")
     else:
         pass
-        #print("I already stole the diamond, I need to get away")
     return Node.STATE_SUCCESS
 
-def tick_stealPainting():
-    global valuableStolen
-    if valuableStolen == ValuableTheftStatus.NONE:
-        valuableStolen = ValuableTheftStatus.PAINTING_STOLEN
+def tick_stealPainting(bb):
+    if bb.valuableStolen == HeistBlackboard.ValuableTheftStatus.NONE:
+        bb.valuableStolen = HeistBlackboard.ValuableTheftStatus.PAINTING_STOLEN
         print("Managed to steal the painting!")
     else:
         pass
         #print("I already stole the diamond, I need to get away")
     return Node.STATE_SUCCESS
 
-def tick_escapeVan():
-    global vanEscaped
-    if not vanEscaped:
-        vanEscaped = True
+def tick_escapeVan(bb):
+    if not bb.vanEscaped:
+        bb.vanEscaped = True
         print("I got away in my van!")
     else:
         pass
         #print("I'm currently getting away in my van")
     return Node.STATE_SUCCESS
 
-def tick_pawnItem():
-    global valuableStolen
-    global balance
+def tick_pawnItem(bb):
 
-    if valuableStolen == ValuableTheftStatus.DIAMOND_STOLEN:
+    if bb.valuableStolen == HeistBlackboard.ValuableTheftStatus.DIAMOND_STOLEN:
         print("Pawned the stolen diamond and made a tidy sum!")
-        balance += 1000000
-    elif valuableStolen == ValuableTheftStatus.PAINTING_STOLEN:
+        bb.balance += 1000000
+    elif bb.valuableStolen == HeistBlackboard.ValuableTheftStatus.PAINTING_STOLEN:
         print("Sold the stolen painting for a pretty penny!")
-        balance += 500000
+        bb.balance += 500000
     else:
         #Set it to a third state to prevent errors when the tree is being re_evaluated
-        valuableStolen = ValuableTheftStatus.ALREADY_SOLD
+        bb.valuableStolen = HeistBlackboard.ValuableTheftStatus.ALREADY_SOLD
 
     return Node.STATE_SUCCESS
 
-def tick_payLawyer():
-    global balance
+def tick_payLawyer(bb):
     print("Paid my lawyer to get me out of prison - he didn't come cheap")
 
-    if balance > 0:
-        balance = round(balance * 0.65)
+    if bb.balance > 0:
+        bb.balance = round(bb.balance * 0.65)
     else:
-        balance -= 300000
+        bb.balance -= 300000
     return Node.STATE_SUCCESS
 
-def tick_leavePrison():
-    global theftDecided
-    global planDevised
-    global vanEscaped
-    global walletChecked
-    global valuableStolen
-    global evadedSecurity
-    global notInPrison
-
+def tick_leavePrison(bb):
     print("Well that sucked! I'm happy to be a free man again.")
-    theftDecided = False
-    planDevised = False
-    vanEscaped = False
-    walletChecked = False
-    evadedSecurity = False
-    notInPrison = True
-    valuableStolen = ValuableTheftStatus.NONE
+    bb.theftDecided = False
+    bb.planDevised = False
+    bb.vanEscaped = False
+    bb.walletChecked = False
+    bb.evadedSecurity = False
+    bb.notInPrison = True
+    bb.valuableStolen = HeistBlackboard.ValuableTheftStatus.NONE
     return Node.STATE_SUCCESS
 
 
-def tick_nextHeist():
-    global theftDecided
-    global planDevised
-    global vanEscaped
-    global walletChecked
-    global valuableStolen
-    global evadedSecurity
-    global notInPrison
-
+def tick_nextHeist(bb):
     print("Great heist guys, let's prepare for the next one")
-    theftDecided = False
-    planDevised = False
-    vanEscaped = False
-    walletChecked = False
-    evadedSecurity = False
-    notInPrison = True
-    valuableStolen = ValuableTheftStatus.NONE
+    bb.theftDecided = False
+    bb.planDevised = False
+    bb.vanEscaped = False
+    bb.walletChecked = False
+    bb.evadedSecurity = False
+    bb.notInPrison = True
+    bb.valuableStolen = HeistBlackboard.ValuableTheftStatus.NONE
     return Node.STATE_SUCCESS
 
 if __name__ == '__main__':
@@ -244,6 +214,8 @@ if __name__ == '__main__':
     ending.addChild(escapeToVan)
     ending.addChild(sellValuable)
     ending.addChild(celebrate)
+
+    tree.bind(HeistBlackboard())
 
     print("\n\nTree Structure")
     print("========================\n")

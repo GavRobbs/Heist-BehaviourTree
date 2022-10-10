af8e128ca1a6dee805a402c9bc005dd5465bf9f0
Gavin <gnation15@gmail.com>
2022-10-09 21:05:31 -0500

    Added type checking for the blackboard passed to Node.bind()

--- core/node.py
+++ core/node.py
@@ -1,6 +1,9 @@
 
 #A generic node class - this is only really used for the root node of the tree.
 #All other nodes are usually one of the other subtypes
+from core.blackboard import Blackboard
+
+
 class Node:
     STATE_SUCCESS = 0
     STATE_FAILED = 1
@@ -28,8 +31,11 @@ class Node:
         return self.children[self.currentChild].process()
 
     def bind(self, blackboard):
-        self.blackboard = blackboard
-        for child in self.children:
-            child.bind(blackboard)
+        if issubclass(blackboard, Blackboard):
+            self.blackboard = blackboard
+            for child in self.children:
+                child.bind(blackboard)
+        else:
+            raise TypeError("Attempting to bind a blackboard to a node which is not derived from Blackboard")
 
     
